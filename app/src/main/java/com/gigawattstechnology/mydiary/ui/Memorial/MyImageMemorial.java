cdc391ac53eae2143589b38dd39787407cfe8e3b
Satwik Mohan <satwikplay69@gmail.com>
2022-10-20 15:05:34 +0530

    MongoDB+Glide+Cloudinary+Firebase+Retrofit+volley+RoomDatabase+Picasso

--- app/src/main/java/com/gigawattstechnology/mydiary/ui/Memorial/MyImageMemorial.java
+++ app/src/main/java/com/gigawattstechnology/mydiary/ui/Memorial/MyImageMemorial.java
@@ -2,6 +2,7 @@ package com.gigawattstechnology.mydiary.ui.Memorial;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 
@@ -9,10 +10,12 @@ import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.preference.PreferenceManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -61,7 +64,14 @@ ImageMemorialAdapter imageMemorialAdapter;
         config.put("api_key","757922249274476");
         config.put("api_secret","vONW2OphHLSaY-eDlZpe74cbuZU");
         config.put("secure", true);
-        MediaManager.init(requireContext(),config);
+        //MediaManager.init(requireContext(),config);
+
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(requireContext());
+        if(prefs.getBoolean("firstRun", true)) {
+           // once(); // <-- your function
+            MediaManager.init(requireContext(),config);
+            prefs.edit().putBoolean("firstRun", false).commit();
+        }
 
         binding=FragmentMyImageMemorialBinding.inflate(inflater,container,false);
         databaseReference= FirebaseDatabase.getInstance().getReference("My Image Memorial");
